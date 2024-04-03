b94a827ae1d1b227ba1b614f8065890b66f2ecc3
Mikhail Kovalenko <kovalenko_m@mirea.ru>
2024-04-03 12:21:45 +0300

    Add experts

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,6 +1,8 @@
 import Expert1 from '@/public/images/expert-1.jpg'
 import Expert2 from '@/public/images/expert-2.jpg'
 import Expert3 from '@/public/images/expert-3.jpg'
+import Expert4 from '@/public/images/expert-4.jpg'
+import Expert5 from '@/public/images/expert-5.jpg'
 import PeopleCard from "@/components/people-card";
 import React from "react";
 import {StaticImageData} from "next/image";
@@ -24,11 +26,15 @@ export default function Testimonials() {
           {/* Testimonials */}
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
           <PeopleCard image={Expert1 as StaticImageData} name="Соня Жеревчук" position="Куратор проекта Indie Go"
-                      text="Инди Геймджем - это не только конкурс, но и возможность для личного и профессионального роста."/>
+                      text="Расскажу как эффективно презентовать проект на питчингах и как правильно к ним подготовиться."/>
             <PeopleCard image={Expert2 as StaticImageData} name="Михаил Коваленко" position="Наставник стартап-проектов IPPO Science"
-                        text="Ментор хакатонов, координатор ВКРСт и стартап-проектов для конкурсов Фонда содействия инновациям."/>
+                        text="Ментор хакатонов, координатор ВКРСт и стартап-проектов для ФСИ, основной организатор Инди Геймджема."/>
             <PeopleCard image={Expert3 as StaticImageData} name="Владимир Вареник" position="Гендиректор Mensa VR"
                         text="Ценность игр как искусства возникает только тогда, когда большое количество людей это признает."/>
+            <PeopleCard image={Expert4 as StaticImageData} name="Варвара Кожинова" position="Indie Varvar's"
+                        text="Расскажу как правильно участвовать в джемах и что поможет реализовать все задумки в такой короткий срок."/>
+            <PeopleCard image={Expert5 as StaticImageData} name="Евгений Медведев" position="Специалист QA"
+                        text="Поделюсь секретами налаживания процессов тестирования за свои 15 лет работы в индустрии."/>
           </div>
         </div>
       </div>
